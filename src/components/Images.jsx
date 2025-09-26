38c4ea1aa3e8064ed2541308077a263ec67e577f
hcruz <hum.alex93@gmail>
2025-09-26 14:31:05 -0600

    React Image Optimization Tutorial

---
+++ src/components/Images.jsx
@@ -0,0 +1,17 @@
+import { Image } from '@imagekit/react';
+
+const Images = ({src, className, w, h, alt}) => {
+  return (
+    <Image  
+      urlEndpoint={import.meta.env.VITE_IK_URL_ENDPOINT} 
+      src= {src} 
+      className= {className}
+      loading = "lazy"
+      alt={alt}
+      width= {w}
+      height={h}
+    />
+  )
+}
+
+export default Images
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import  Images  from "./Images"
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -6,7 +7,7 @@ const Navbar = () => {
     <div className='w-full h-16 md:h-20 flex items-center justify-between'>
       {/* LOGO */}
       <div className="flex items-center gap-4 text-2xl font-bold">
-        <img src="/logo.png" className="w-8 h-8" alt=""/>
+        <Images src="logo.png" alt="lama Logo" w={32} h={32}/>
         <span>lamalog</span>
       </div>
       {/* MOBILE MENU */}
