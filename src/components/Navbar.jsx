65cdaa0c6d4ee49e31629318d07e7e76ba0222ec
hcruz <hum.alex93@gmail>
2025-09-29 11:55:30 -0600

    5 React Router Dom Outlet Tutorial

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react"
 import  Images  from "./Images"
+import { Link } from "react-router";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   return (
     <div className='w-full h-16 md:h-20 flex items-center justify-between'>
       {/* LOGO */}
-      <div className="flex items-center gap-4 text-2xl font-bold">
+      <Link to="/" className="flex items-center gap-4 text-2xl font-bold">
         <Images src="logo.png" alt="lama Logo" w={32} h={32}/>
         <span>lamalog</span>
-      </div>
+      </Link>
       {/* MOBILE MENU */}
       <div className="md:hidden">
         {/*MOBILE BUTTON*/}
@@ -24,24 +25,24 @@ const Navbar = () => {
           open ? "-right-0" :  "-right-[100%]"
           }`}
         >
-          <a href="/">Home</a>
-          <a href="/">Trending</a>
-          <a href="/">Most popular</a>
-          <a href="/">About</a>
-          <a>
+          <Link >Home</Link>
+          <Link to="/" >Trending</Link>
+          <Link to="/" >Most popular</Link>
+          <Link to="/" >About</Link>
+          <Link to="">
             <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white ">Login 👋</button>
-          </a>
+          </Link>
         </div>
       </div>
       {/*DESKTOP MENU*/}
       <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium ">
-        <a href="/">Home</a>
-        <a href="/">Trending</a>
-        <a href="/">Most popular</a>
-        <a href="/">About</a>
-        <a>
+        <Link to="/" >Home</Link>
+        <Link to="/" >Trending</Link>
+        <Link to="/" >Most popular</Link>
+        <Link to="/" >About</Link>
+        <Link to="">
           <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white ">Login 👋</button>
-        </a>
+        </Link>
       </div>
     </div>
   )
