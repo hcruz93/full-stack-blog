7b8e8b590ec567855965b434288d8d61e2fa4fa8
hcruz <hum.alex93@gmail>
2025-09-29 12:55:07 -0600

    React 19 Clerk Authentication Tutorial

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import  Images  from "./Images"
-import { Link } from "react-router";
+import { Link } from "react-router"
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react"
+
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -40,9 +42,14 @@ const Navbar = () => {
         <Link to="/" >Trending</Link>
         <Link to="/" >Most popular</Link>
         <Link to="/" >About</Link>
-        <Link to="">
-          <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white ">Login 👋</button>
-        </Link>
+        <SignedOut>
+          <Link to="/login">
+            <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white ">Login 👋</button>
+          </Link>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,16 @@ import Write from './routes/Write.jsx'
 import LoginPage from './routes/LoginPage.jsx'
 import RegisterPage from './routes/RegisterPage.jsx'
 import SinglePostPage from './routes/SinglePostPage.jsx'
-import MainLayout from './layouts/MainLayout.jsx';
+import MainLayout from './layouts/MainLayout.jsx'
+
+import { ClerkProvider } from '@clerk/clerk-react';
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
 
 
 const router = createBrowserRouter([
@@ -51,6 +60,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <RouterProvider router={router} />
+    </ClerkProvider>
   </StrictMode>,
 )
