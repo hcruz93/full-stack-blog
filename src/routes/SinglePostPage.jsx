307fa0e82ec630e25637eeafac2461ade23a3f31
hcruz <hum.alex93@gmail>
2025-10-08 16:59:36 -0600

    React Single Post Page Design

--- src/routes/SinglePostPage.jsx
+++ src/routes/SinglePostPage.jsx
@@ -1,6 +1,129 @@
+import Images from "../components/Images"
+import { Link } from "react-router"
+import PostMenuActions from "../components/PostMenuActions"
+import Search from "../components/Search"
+
 const SinglePostPage = () => {
   return (
-    <div className="">SinglePostPage</div>
+    <div className="flex flex-col gap-8">
+      {/* detail */}
+      <div className="flex gap-8">
+        <div className="lg:w-3/5 flex flex-col gap-8">
+          <h1 className="text-xl md:text-3xl xl:text-4xl 2xl:text-5xl font-semibold">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.?
+          </h1>
+          <div className="flex items-center gap-2 text-gray-400 text-sm">
+            <span>Written by</span>
+            <Link className="text-blue-800">John Doe</Link>
+            <span>on</span>
+            <Link className="text-blue-800">Web design</Link>
+            <span>2 days ago</span>
+          </div>
+          <p className="text-gray-500 font-medium">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Id odio eum earum cum porro eos soluta quasi non dolorum, 
+            numquam libero rem. Eveniet nisi ea ut quia
+          </p>
+        </div>
+        <div className="hidden lg:block w-2/5">
+          <Images src="postImg.jpeg" w="600" className="rounded-2xl"/>
+        </div>
+      </div>
+      {/* content */}
+      <div className="flex flex-col md:flex-row gap-12">
+        {/* text */}
+        <div className="lg:text-lg flex flex-col gap-6 text-justify">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Assumenda ullam, odio autem labore totam doloribus ad soluta pariatur 
+            quaerat exercitationem sapiente, recusandae illo repudiandae aliquam 
+            quam saepe fugit eum ipsum!
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Assumenda ullam, odio autem labore totam doloribus ad soluta pariatur 
+            quaerat exercitationem sapiente, recusandae illo repudiandae aliquam 
+            quam saepe fugit eum ipsum!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Assumenda ullam, odio autem labore totam doloribus ad soluta pariatur 
+            quaerat exercitationem sapiente, recusandae illo repudiandae aliquam 
+            quam saepe fugit eum ipsum!
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Assumenda ullam, odio autem labore totam doloribus ad soluta pariatur 
+            quaerat exercitationem sapiente, recusandae illo repudiandae aliquam 
+            quam saepe fugit eum ipsum!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Assumenda ullam, odio autem labore totam doloribus ad soluta pariatur 
+            quaerat exercitationem sapiente, recusandae illo repudiandae aliquam 
+            quam saepe fugit eum ipsum!
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Assumenda ullam, odio autem labore totam doloribus ad soluta pariatur 
+            quaerat exercitationem sapiente, recusandae illo repudiandae aliquam 
+            quam saepe fugit eum ipsum!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Assumenda ullam, odio autem labore totam doloribus ad soluta pariatur 
+            quaerat exercitationem sapiente, recusandae illo repudiandae aliquam 
+            quam saepe fugit eum ipsum!
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Assumenda ullam, odio autem labore totam doloribus ad soluta pariatur 
+            quaerat exercitationem sapiente, recusandae illo repudiandae aliquam 
+            quam saepe fugit eum ipsum!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Assumenda ullam, odio autem labore totam doloribus ad soluta pariatur 
+            quaerat exercitationem sapiente, recusandae illo repudiandae aliquam 
+            quam saepe fugit eum ipsum!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Assumenda ullam, odio autem labore totam doloribus ad soluta pariatur 
+            quaerat exercitationem sapiente, recusandae illo repudiandae aliquam 
+            quam saepe fugit eum ipsum!
+          </p>
+        </div>
+        {/* manu */}
+        <div className="px-4 h-max sticky top-8">
+          <h1 className="mb-4 text-sm font-medium">Author</h1>
+          <div className="flex flex-col gap-4">
+            <div className="flex items-center gap-8">
+              <Images 
+                src="userImg.jpeg" 
+                className="w-12 h-12 rounded-full object-cover" 
+                w="48" 
+                h="48"
+              />
+              <Link className="text-blue-800">John Doe</Link>
+            </div>
+            <p className="text-sm text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit.?</p>
+            <div className="flex gap-2">
+              <Link>
+                <Images src="facebook.svg"/>
+              </Link>
+              <Link>
+                <Images src="instagram.svg"/>
+              </Link>
+            </div>
+          </div>
+          <PostMenuActions/>
+          <h1  className="mt-8 mb-4 text-sm font-medium">Categories</h1>
+          <div className="flex flex-col gap-2 text-sm">
+            <Link className="underline">All</Link>
+            <Link className="underline">Web Design</Link>
+            <Link className="underline">Development</Link>
+            <Link className="underline">Databases</Link>
+            <Link className="underline">Search Engines</Link>
+            <Link className="underline">Marketing</Link>
+          </div>
+          <h1 className="mt-8 mb-4 text-sm font-medium">Search</h1>
+          <Search/>
+        </div>
+      </div>
+    </div> 
   )
 }
 
