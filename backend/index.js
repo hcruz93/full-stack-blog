29bdf9654b41f5e3400bfbcfceaca8bae69e6e95
hcruz <hum.alex93@gmail>
2025-10-15 14:14:05 -0600

    Express 5 Rest API Tutorial (Routes, Models, Controllers)

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,17 @@
 import express from "express"
-
+import userRouter from "./routes/user.route.js"
+import postRouter from "./routes/post.route.js"
+import commentRouter from "./routes/comment.route.js"
 const app = express()
 
-console.log(process.env.test);
+// app.get("/test",(req, res)=>{
+//   res.status(200).send("it works!")
+// })
+
+app.use("/users", userRouter)
+app.use("/posts", postRouter)
+app.use("/comments", commentRouter)
+
 app.listen(3000,() => {
   console.log("Server is running!");
 })
