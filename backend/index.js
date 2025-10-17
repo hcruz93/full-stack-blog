689c690e74af9e51ced7c101d92cb7ced98ace04
hcruz <hum.alex93@gmail>
2025-10-17 08:25:37 -0600

    React 19 Express 5 MongoDB Tutorial with

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 import express from "express"
+import connectDB from "./lib/connect.DB.js"
 import userRouter from "./routes/user.route.js"
 import postRouter from "./routes/post.route.js"
 import commentRouter from "./routes/comment.route.js"
+
 const app = express()
 
 // app.get("/test",(req, res)=>{
@@ -13,5 +15,6 @@ app.use("/posts", postRouter)
 app.use("/comments", commentRouter)
 
 app.listen(3000,() => {
+  connectDB()
   console.log("Server is running!");
 })
