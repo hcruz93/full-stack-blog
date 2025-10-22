44f65a0913df72eb389bb67a8ab43f4e24adbb47
hcruz <hum.alex93@gmail>
2025-10-22 12:26:55 -0600

    MERN Stack CRUD Tutorial

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import postRouter from "./routes/post.route.js"
 import commentRouter from "./routes/comment.route.js"
 
 const app = express()
+app.use(express.json())
 
 // app.get("/test",(req, res)=>{
 //   res.status(200).send("it works!")
--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -32,7 +32,7 @@ const postSchema = new Schema(
       default:false,
     },
     vist:{
-      type:Boolean,
+      type:Number,
       default:0,
     },
   },
