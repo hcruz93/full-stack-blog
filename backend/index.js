a24e35480ca4338dc065b1419d99da8d43a07e14
hcruz <hum.alex93@gmail>
2025-10-27 13:30:49 -0600

    Express 5 Error Handling Tutorial

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,17 @@ app.use("/users", userRouter)
 app.use("/posts", postRouter)
 app.use("/comments", commentRouter)
 
+app.use((error, req, res, next)=>{
+
+  res.status(error.status || 500)
+
+  res.json({
+    message:error.message || "Something went wrong",
+    status: error.status,
+    stack: error.stack,
+  })
+})
+
 app.listen(3000,() => {
   connectDB()
   console.log("Server is running!");
