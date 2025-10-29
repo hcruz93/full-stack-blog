1783aac287af6725865e5f049770ef5d503cdaa0
hcruz <hum.alex93@gmail>
2025-10-29 12:41:58 -0600

    How to Add Clerk Users to a Database (Clerk Webhook Tutorial)

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,10 @@ import connectDB from "./lib/connect.DB.js"
 import userRouter from "./routes/user.route.js"
 import postRouter from "./routes/post.route.js"
 import commentRouter from "./routes/comment.route.js"
+import webhookRouter from "./routes/webhook.route.js"
 
 const app = express()
+app.use("/webhooks", webhookRouter)
 app.use(express.json())
 
 // app.get("/test",(req, res)=>{
@@ -14,6 +16,7 @@ app.use(express.json())
 app.use("/users", userRouter)
 app.use("/posts", postRouter)
 app.use("/comments", commentRouter)
+
 
 app.use((error, req, res, next)=>{
 
