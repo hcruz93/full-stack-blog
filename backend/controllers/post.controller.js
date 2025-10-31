5bdf363cabd6bbb34868d3f3cfb3dac3a3789714
hcruz <hum.alex93@gmail>
2025-10-31 14:45:20 -0600

    Express 5 Clerk Authorization (Protecting Routes)

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,4 +1,5 @@
 import Post from "../models/post.model.js"
+import User from "../models/user.model.js"
 
 export const getPosts = async (req, res) => {
   const posts = await Post.find()
@@ -11,15 +12,42 @@ export const getPost = async (req, res) => {
 };
 
 export const createPost = async (req, res) => {
-  const newPost = new Post(req.body)
+
+  const clerkUserId = req.auth.userId
+
+  if (!clerkUserId) {
+    return res.status(401).json("Not authenticated!")
+  }
+
+  const user = await User.findOne({clerkUserId})
+
+  if (!user) {
+    return res.status(404).json("User not found!")
+  }
+
+  const newPost = new Post({ user:user._id, ...req.body })
 
   const post = await newPost.save()
   res.status(200).json(post)
 };
 
 export const deletePost = async (req, res) => {
-  const newPost = new Post(req.body)
 
-  const post = await Post.findByIdAndDelete(req.params.id)
+  const clerkUserId = req.auth.userId
+
+  if (!clerkUserId) {
+    return res.status(401).json("Not authenticated!")
+  }
+
+  const user = await User.findOne({ clerkUserId })
+
+   const Post = await Post.findOneAndDelete({ 
+    _id: req.params.id, 
+    user:user._id 
+  })
+
+  // const newPost = new Post(req.body)
+
+  // const post = await Post.findByIdAndDelete(req.params.id)
   res.status(200).json("Post has been deleted")
 };
