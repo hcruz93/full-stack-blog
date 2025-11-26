a144238fc74f7cf073719cc5e7eff4020bfb4a48
hcruz <hum.alex93@gmail>
2025-11-26 16:54:14 -0600

    React 19 Data Fetching with React Query

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,8 +4,6 @@ import './index.css'
 // import App from './App.jsx'
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
-
-
 import Homepage from './routes/Homepage.jsx'
 import PostListPage from './routes/PostListPage.jsx'
 import Write from './routes/Write.jsx'
@@ -13,8 +11,13 @@ import LoginPage from './routes/LoginPage.jsx'
 import RegisterPage from './routes/RegisterPage.jsx'
 import SinglePostPage from './routes/SinglePostPage.jsx'
 import MainLayout from './layouts/MainLayout.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
-import { ClerkProvider } from '@clerk/clerk-react';
+const queryClient = new QueryClient()
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -61,7 +64,9 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+    </QueryClientProvider>
     </ClerkProvider>
   </StrictMode>,
 )
