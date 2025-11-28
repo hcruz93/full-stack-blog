e75b2a2d7b8d58d088e451dea49c71eb5791fa62
hcruz <hum.alex93@gmail>
2025-11-28 15:08:01 -0600

    React 19 Data Mutations with React Query

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -16,6 +16,8 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const queryClient = new QueryClient()
 
@@ -66,6 +68,7 @@ createRoot(document.getElementById('root')).render(
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <ToastContainer position='bottom-right'/>
     </QueryClientProvider>
     </ClerkProvider>
   </StrictMode>,
--- client/src/routes/Write.jsx
+++ client/src/routes/Write.jsx
@@ -1,10 +1,36 @@
-import { useUser } from "@clerk/clerk-react"
+import { useAuth, useUser } from "@clerk/clerk-react"
 import 'react-quill-new/dist/quill.snow.css';
 import ReactQuill from "react-quill-new"
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import { toast } from "react-toastify";
+// import Upload from "../components/Upload";
 
 
 const write = () => {
   const {isLoaded, isSignedIn} = useUser()
+  const [value, setValue] = useState('')
+
+  const navigate = useNavigate()
+
+  const {getToken} = useAuth()
+
+  const mutation = useMutation({
+    mutationFn: async (newPost) => {
+      const token = await getToken()
+      return axios.post(`${import.meta.env.VITE_API_URL}/posts`, newPost, {
+        headers:{
+          Authorization: `Bearer ${token}`,
+        }
+      })
+    },
+    onSuccess:(res)=>{
+      toast.success("Post has been created")
+      navigate(`/${res.data.slug}`);
+    }
+  })
 
   if (!isLoaded) {
     return <div className="">Loading...</div>
@@ -12,15 +38,44 @@ const write = () => {
   if (isLoaded && !isSignedIn) {
     return <div className="">Yoy should login!</div>
   }
+
+  const handleSubmit = e=>{
+    e.preventDefault()
+    const formData = new FormData(e.target)
+
+    const data ={
+      title:formData.get("title"),
+      category:formData.get("category"),
+      desc:formData.get("desc"),
+      content: value,
+    }
+
+    console.log(data)
+    mutation.mutate(data)
+  }
+
   return (
     <div className="h-[calc(100vh-64px)] md:h-[calc(100vh-80px)] flex flex-col gap-6">
       <h1 className="text-cl font-light">Create a New Post</h1>
-      <form className="flex flex-col gap-6 flex-1 mb-6">
-        <button className="w-max p-2 shadow-md rounded-xl text-sm text-gray-500 bg-white">Add a cover image</button>
-        <input className="text-4xl font-semibold bg-transparent outline-none" type="text" placeholder="My Awesome Story" />
+      <form onSubmit={handleSubmit} className="flex flex-col gap-6 flex-1 mb-6">
+        <button className="w-max p-2 shadow-md rounded-xl text-sm text-gray-500 bg-white">
+          Add a cover image
+        </button>
+        <input 
+          className="text-4xl font-semibold bg-transparent outline-none" 
+          type="text" 
+          placeholder="My Awesome Story" 
+          name= "title"
+        />
         <div className="flex items-center gap-4">
-          <label htmlFor="" className="text-sm">Choose a category:</label>
-          <select name="cat" id="" className="p-2 rounded-xl bg-white shadow-md">
+          <label htmlFor="" className="text-sm">
+            Choose a category:
+          </label>
+          <select 
+            name="category" 
+            id="" 
+            className="p-2 rounded-xl bg-white shadow-md"
+          >
             <option value="General">General</option>
             <option value="web-design">Web Design</option>
             <option value="development">Development</option>
@@ -29,9 +84,31 @@ const write = () => {
             <option value="marketing">Marketing</option>
           </select>
         </div>
-        <textarea className="p-4 rounded-xl bg-white shadow-md" name="desc" placeholder="A short Description"/>
-        <ReactQuill theme="snow" className="flex-1 rounded-xl bg-white shadow-md"/>
-        <button className="bg-blue-800 text-white font-medium rounded-xl mt-4 p-2 w-36">Send</button>
+        <textarea 
+          className="p-4 rounded-xl bg-white shadow-md"  
+          name="desc" 
+          placeholder="A short Description"
+        />
+        <div className="flex flex-1 ">
+          <div className="flex flex-col gap-2 mr-2">
+            <div className="cursor-pointer">🌆</div>
+            <div className="cursor-pointer">▶️</div>
+          </div>
+          <ReactQuill
+            theme="snow"
+            className="flex-1 rounded-xl bg-white shadow-md"
+            value={value}
+            onChange={setValue}
+            // readOnly={0 < progress && progress < 100}
+          />
+        </div>
+        <button 
+          disabled={mutation.isPending} 
+          className="bg-blue-800 text-white font-medium rounded-xl mt-4 p-2 w-36 disabled:bg-blue-400 disabled:cursor-not-allowed"
+        >
+          {mutation.isPending ? "Loading..." : "Send"}
+        </button>
+        {mutation.isError && <span>{mutation.error.message}</span>}
       </form>
     </div>
   )
